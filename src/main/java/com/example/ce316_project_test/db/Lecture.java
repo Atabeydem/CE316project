382d6fffe00cc47675ed7ae00c43f4c87d31145a
Atabey Demir <85640435+Atabeydem@users.noreply.github.com>
2023-05-15 18:40:03 +0300

    Add files via upload
    DB connection done, GUI upgraded and also Configuration solved.

---
+++ src/main/java/com/example/ce316_project_test/db/Lecture.java
@@ -0,0 +1,43 @@
+
+package com.example.ce316_project_test.db;
+
+public class Lecture {
+    private int id;
+    private String name;
+    private String lecturer;
+
+    public Lecture()
+    {
+
+    }
+
+    public Lecture(int id, String name, String lecturer) {
+        this.id = id;
+        this.name = name;
+        this.lecturer = lecturer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+}
